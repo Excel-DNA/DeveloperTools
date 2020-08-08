28c785a617e946892b3575335a41773503c2b958
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2020-08-08 14:36:19 +0500

    Created VB project template.

---
+++ ExcelDNAVSExtension/VBProjectTemplate/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBProjectTemplate")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Company")>
+<Assembly: AssemblyProduct("VBProjectTemplate")>
+<Assembly: AssemblyCopyright("Copyright © Company 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ed4b53f2-48f6-4b3d-a5e5-226d5b3c2e7d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
