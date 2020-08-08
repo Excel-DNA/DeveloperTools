dcd7eb0b517ba2ef893b22de98e800c79f58c7e7
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2020-08-08 21:32:53 +0500

    Created an empty project creation wizard.

---
+++ ExcelDNAVSExtension/ExcelDNAVSExtension/TemplateWizard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TemplateWizard;
+using EnvDTE;
+
+namespace ExcelDNAVSExtension
+{
+    public class TemplateWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
