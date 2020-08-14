853222e38ba2c77ce7c3ae31791f5f44456e61bb
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2020-08-14 21:38:13 +0500

    Included a ribbon sample to the VB project template controllable from the wizard.

--- ExcelDNAVSExtension/CSProjectTemplate/MyRibbon.cs
+++ ExcelDNAVSExtension/CSProjectTemplate/MyRibbon.cs
@@ -8,8 +8,7 @@ public class MyRibbon : ExcelRibbon
 {
     public override string GetCustomUI(string RibbonID)
     {
-        using (System.IO.Stream stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(
-            "$safeprojectname$." + "MyRibbon.xml"))
+        using (System.IO.Stream stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("$safeprojectname$.MyRibbon.xml"))
         {
             using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
             {
---
+++ ExcelDNAVSExtension/VBProjectTemplate/MyRibbon.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+Imports ExcelDna.Integration.CustomUI
+
+<ComVisible(True)>
+Public Class MyRibbon
+    Inherits ExcelRibbon
+
+    Public Overrides Function GetCustomUI(RibbonID As String) As String
+        Using stream As Stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("$safeprojectname$.MyRibbon.xml")
+            Using reader As New StreamReader(stream)
+                Return reader.ReadToEnd()
+            End Using
+        End Using
+    End Function
+
+    Public Sub OnButtonPressed(control As IRibbonControl)
+        System.Windows.Forms.MessageBox.Show("Hello!")
+    End Sub
+
+End Class
