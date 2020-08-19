15816feebf5622fd934686bdce133dad5cff87f6
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2020-08-19 11:46:30 +0500

    Created C# ribbon item template.

---
+++ ExcelDNAVSExtension/CSRibbonTemplate/Ribbon.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using ExcelDna.Integration.CustomUI;
+
+namespace $rootnamespace$
+{
+    [ComVisible(true)]
+public class $safeitemrootname$ : ExcelRibbon
+{
+    public override string GetCustomUI(string RibbonID)
+{
+    return $fileinputname$Resources.Ribbon;
+}
+
+public override object LoadImage(string imageId)
+{
+    // This will return the image resource with the name specified in the image='xxxx' tag
+    return $fileinputname$Resources.ResourceManager.GetObject(imageId);
+}
+
+public void OnButtonPressed(IRibbonControl control)
+{
+    System.Windows.Forms.MessageBox.Show("Hello!");
+}
+}
+}
