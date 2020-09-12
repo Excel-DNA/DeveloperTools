7fa0c2c8086789bfea6223cb12f2aafa102c10df
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2020-09-12 13:41:52 +0500

    Added a ribbon to the Examples add-in.

---
+++ ExcelDna.Testing/Examples/MyRibbon.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using ExcelDna.Integration;
+using ExcelDna.Integration.CustomUI;
+using Microsoft.Office.Interop.Excel;
+
+namespace Examples
+{
+    [ComVisible(true)]
+    public class MyRibbon : ExcelRibbon
+    {
+        public override string GetCustomUI(string RibbonID)
+        {
+            return RibbonResources.Ribbon;
+        }
+
+        public override object LoadImage(string imageId)
+        {
+            // This will return the image resource with the name specified in the image='xxxx' tag
+            return RibbonResources.ResourceManager.GetObject(imageId);
+        }
+
+        public void OnButtonPressed(IRibbonControl control)
+        {
+            Application app = (Application)ExcelDnaUtil.Application;
+            Range targetRange = app.ActiveSheet.Range["A1:C2"];
+
+            object[,] newValues = new object[,] { { "One", 2, "Three" }, { true, System.DateTime.Now, "" } };
+            targetRange.Value = newValues;
+        }
+    }
+}
